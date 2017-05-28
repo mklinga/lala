b3793d864e8197755a23292023f6eff5f1183810
Markus Klinga <laite@gmx.com>
2017-05-28 11:13:15 +0200

    Tulem ukaan

--- html/media.php
+++ html/media.php
@@ -2,17 +2,22 @@
 
 
 <section id='lala-mediassa' class='block'>
-  <h2>LaLa kiinnostaa myös mediaa</h2>
-  <p>
-    Tässä poimintoja meistä tehdyistä lehtijutuista.
-  </p>
+  <div>
+    <h2>LaLa kiinnostaa myös mediaa</h2>
+    <p>
+      Tässä poimintoja meistä tehdyistä lehtijutuista.
+    </p>
 
-  <div class='context-item'>
-    <ul>
-      <li><a href='/assets/pdf/Hilkka_muisti.pdf'>Hilkka muisti laulun sanat</a></li>
-      <li><a href='/assets/pdf/Laitoslaulajat.pdf'>Laulu vain hänelle</a></li>
-      <li><a href='/assets/pdf/seura.pdf'>Laulettaisiinko vähän?</a></li>
-    </ul>
+    <div class='context-item'>
+      <ul>
+        <li><a href='/assets/pdf/Hilkka_muisti.pdf'>Hilkka muisti laulun sanat</a></li>
+        <li><a href='/assets/pdf/Laitoslaulajat.pdf'>Laulu vain hänelle</a></li>
+        <li><a href='/assets/pdf/seura.pdf'>Laulettaisiinko vähän?</a></li>
+      </ul>
+    </div>
+  </div>
+  <div>
+    <img src='assets/mediassa.png' />
   </div>
 </section>
 
--- html/tulemukaan.php
+++ html/tulemukaan.php
@@ -1,20 +1,33 @@
 <?php include('head.php'); ?>
 
-<section id='tule-mukaan' class='block'>
-  <h2>Tule mukaan!</h2>
-  <h3>Liity jäseneksi!</h3>
-  <p>
-  Helpoin tapa tukea toimintaamme on liittyä kannatusjäseneksi. Kannatusjäsenen vuosittaisella 50 euron maksulla saat kerran vuodessa uutiskirjeen, jossa kerromme kuulumisistamme.
-  Varsinaiseksi jäseneksi voit liittyä käytyäsi Laitoslaulajat ry:n viikonloppuvalmennuksen. Jäsenmaksu on 15 euroa.
-  </p>
-  <h3>Tilaa laulupaketti!</h3>
-  <p>
-  Hanki laitoslaulajan viiden kerran paketti tutulle vanhukselle. Tilauskaavakkeen löydät <a href='/tilaa.php'>täältä</a>.
-  </p>
-  <h3>Ryhdy sponsoriksi!</h3>
-  <p>
-    Voit kohdistaa sponsoroinnin itsellesi ja yrityksellesi tärkeään laitokseen tai tietylle henkilölle. Kohdattavan saat valita itse, mutta voit myös antaa kotihoidon tai laitoksen henkilökunnan valita henkilön, jolle laululahjasta olisi erityisesti iloa.
-  </p>
-</section>
+<div id='tule-mukaan'>
+  <section class='block'>
+    <h2>Tule mukaan!</h2>
+    <h3>Liity jäseneksi!</h3>
+    <p>
+    Helpoin tapa tukea toimintaamme on liittyä kannatusjäseneksi. Kannatusjäsenen vuosittaisella 50 euron maksulla saat kerran vuodessa uutiskirjeen, jossa kerromme kuulumisistamme.
+    Varsinaiseksi jäseneksi voit liittyä käytyäsi Laitoslaulajat ry:n viikonloppuvalmennuksen. Jäsenmaksu on 15 euroa.
+    </p>
+  </section>
+  <section class='image-block block'>
+    <div class='images'>
+      <div><img src="assets/tulemukaan02.png" alt=""/></div>
+      <div><img src="assets/tulemukaan01.png" alt=""/></div>
+      <div class='purple-haze'>&nbsp;</div>
+    </div>
+    <div class='image-block-header'>
+      <h2>Tilaa laulupaketti!</h2>
+    </div>
+    <div class='image-block-text'>
+      <p>Hanki laitoslaulajan viiden kerran paketti tutulle vanhukselle. Tilauskaavakkeen löydät <a href='/tilaa.php'>täältä</a>.</p>
+    </div>
+  </section>
+  <section class='block'>
+    <h3>Ryhdy sponsoriksi!</h3>
+    <p>
+      Voit kohdistaa sponsoroinnin itsellesi ja yrityksellesi tärkeään laitokseen tai tietylle henkilölle. Kohdattavan saat valita itse, mutta voit myös antaa kotihoidon tai laitoksen henkilökunnan valita henkilön, jolle laululahjasta olisi erityisesti iloa.
+    </p>
+  </section>
+</div>
 
 <?php include('footer.php'); ?>
